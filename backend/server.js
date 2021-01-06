ad7711b609cc338d8a9d4bcc55e145a2d15aabd0
AlidaSchefers <alidadschefers@gmail.com>
2021-01-06 18:57:29 -0500

    added imported databaseConnect.js exports

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const server = express() //new express.Express
+const databaseConnect = require('./databaseConnect') //this variable is a function
 
 require('dotenv').config() //make sure this line is before you try accessing any env vars.
+
+databaseConnect() //running the function. connects us to the database and logs various messages
 
 server.use((req, res) => {
     res.send("Hello, World")
