451afd9d80d9a68c81e924202d0550cdc2dcffab
AlidaSchefers <alidadschefers@gmail.com>
2021-01-12 22:39:58 -0500

    added routes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const server = express() //new express.Express
 const databaseConnect = require('./databaseConnect') //this variable is a function
+const usersRouter = require('./routes/users')
 
 require('dotenv').config() //make sure this line is before you try accessing any env vars.
 
 databaseConnect() //running the function. connects us to the database and logs various messages
 
-server.use((req, res) => {
+server.use('/greeting', (req, res, next) => {
     res.send("Hello, World")
+    next()
 })
+
+server.use(express.json()) //allows us to read the request body when it is json format
+server.use('/users', usersRouter)
 
 server.listen(3000, () => { //can update to make dynamic
     console.log("Listen on port 3000")
