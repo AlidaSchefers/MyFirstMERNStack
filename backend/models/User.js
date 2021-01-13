cc02d09f60605dc1975a6c603b9fee5c55387385
AlidaSchefers <alidadschefers@gmail.com>
2021-01-12 22:39:10 -0500

    added exports

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,4 +5,6 @@ const UserSchema = new mongoose.Schema({
     email: {type: String, required: true, minlength: 5, maxlength: 256},
     password: {type: String, required: true, minlength: 6, maxlength: 100},
 })
-module.exports = mongoose.model("users", UserSchema)
+module.exports = mongoose.model('user', UserSchema)
+//btw the collection is named now 'users' b/c MongoDB adds the s to make it plural.
+//schema is the blueprint/set of rules that define documents are structured. model is an object to access and modify the database or collection.
