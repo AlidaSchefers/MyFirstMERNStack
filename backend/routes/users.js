82339283ebf5dfa17dd0f8a9da935ba2808ea765
AlidaSchefers <alidadschefers@gmail.com>
2021-01-16 22:18:27 -0500

    added a login request

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,11 @@
 const router = require('express').Router()
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const sendErr = require('../middlwares/sendGenericError')
 const User = require('../models/User.js') //the model
 const {check, validationResult} = require('express-validator')
+const validator = require('validator')
+const { findOne } = require('../models/User.js')
 
 router.get('/all', async (req, res) => {
     try{
@@ -37,7 +40,7 @@ router.post(
     check('password', "Password must be between 6 and 100 characters").isLength({min: 6, max: 100}),
     async (req, res) => {
     const errors = validationResult(req).array()
-    // console.log(errors)
+    // console.log(errors) //if the email is an email.
     if(errors.length != 0) {
         return res.status(400).json({errors}) //return an array of all the errors (each error is an object)
     }
@@ -45,9 +48,28 @@ router.post(
     try {
         req.body.password = await bcrypt.hash(req.body.password, 10)
         const newUser = await User.create(req.body) //create method is combo of new User and .save() in one
+        //can make new object with the properties (e.g. email, username, password). 
+        //extract just the values you want from the user. 
         res.status(201).json(newUser)
     } catch (err) {
         sendErr(err, res) // this is an un-handled error.
+    }
+})
+
+//error occurs!
+router.post('/login', async (req, res) => { //post put patch all send a body. 
+    try {
+        const DBUser =  validator.isEmail(req.body.identification) ? 
+            User.find({"email": req.body.identification}) :
+            User.find({"username": req.body.identification})
+        console.log("test", DBUser)
+        // if(bcrypt.compare(req.body.password, DBUser.password)) {
+        //     const token = jwt.sign({"id": DBUser.__id}, process.env.SECRET_KEY)
+        //     // const token = jwt.sign({"id": DBUser.__id, "static value": 3}, process.env.SECRET_KEY)
+        //     res.json(token)
+        // }
+    }catch (error) {
+        res.json({message: error.message || error})
     }
 })
 
