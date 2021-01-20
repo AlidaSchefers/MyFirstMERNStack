b3bedaf1177b33435b0fcf09d9cf7c1a3dbaecfb
AlidaSchefers <alidadschefers@gmail.com>
2021-01-19 22:23:41 -0500

    added to post request

--- backend/routes/accountinfo.js
+++ backend/routes/accountinfo.js
@@ -1,10 +1,16 @@
 const router = require('express').Router()
 const verify = require('../middlwares/verifyToken')
+const sendErr = require('../middlwares/sendGenericError')
 
-router.get('/accountinfo', (req,res,next) => {
-    // res.send(req.token)
-    console.log(`token: ${req.token}`)
-    next()
+router.post('/accountinfo', async (req,res,next) => {
+    try {
+        const token = await req.token
+        console.log(`token: ${token}`)
+        res.json(token)
+        next()    
+    } catch (error) {
+        sendErr(err, res)
+    }
 })
 
 
@@ -13,13 +19,8 @@ router.get('/accountinfo', (req,res,next) => {
 //     // const auth_token = req.body.auth_token
 //     console.log("we've got through!")
 //     //const {auth_token} = req.body //this works too, right?
-
-
 // })
-
 //need to have the token match
-
-
 //then need to display info from the database
 
 module.exports = router
