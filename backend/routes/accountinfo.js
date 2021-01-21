4a9a58f3a70c9d14db42fcb220d804a2bb67c5a4
AlidaSchefers <alidadschefers@gmail.com>
2021-01-20 22:39:07 -0500

    added get request displaying user's info

--- backend/routes/accountinfo.js
+++ backend/routes/accountinfo.js
@@ -1,26 +1,19 @@
 const router = require('express').Router()
 const verify = require('../middlwares/verifyToken')
 const sendErr = require('../middlwares/sendGenericError')
+const User = require('../models/User.js')
+// const jwt = require('jsonwebtoken')
 
-router.post('/accountinfo', async (req,res,next) => {
+router.get('/', verify, async (req,res,next) => {
     try {
-        const token = await req.token
-        console.log(`token: ${token}`)
-        res.json(token)
-        next()    
+        // const userId = await jwt.verify(req.body.token, process.env.SECRET_KEY)._id
+        const userId = await req.user._id //from the verify function that checks the token
+        const userInfo = await User.findOne({"_id": userId})
+        // console.log(userInfo)
+        res.send(userInfo)  
     } catch (error) {
-        sendErr(err, res)
+        sendErr(error, res)
     }
 })
-
-
-// router.get('/accountinfo', verify, (req,res,next) => {
-//     //display account info here. when get the info from the database
-//     // const auth_token = req.body.auth_token
-//     console.log("we've got through!")
-//     //const {auth_token} = req.body //this works too, right?
-// })
-//need to have the token match
-//then need to display info from the database
 
 module.exports = router
