7e0ec25ba2d74de036179d6ae0437189803c2af0
AlidaSchefers <alidadschefers@gmail.com>
2021-01-20 22:39:44 -0500

    modified jwt verification

--- backend/middlwares/verifyToken.js
+++ backend/middlwares/verifyToken.js
@@ -2,11 +2,11 @@ const jwt = require('jsonwebtoken')
 
 module.exports = function(req, res, next) { //verifies the token
     //get the token
-    const token = req.token
+    const token = req.body.token
     if(!token) return res.status(401).send('Access Denied')
     try {
-        const verified = jwt.verify(token, process.env.SECRET_KEY)
-        console.log(`verified: ${verified}`)
+        const verified = jwt.verify(token, process.env.SECRET_KEY) //this give us the payload (_id and iat)
+        req.user = verified
         next()
     } catch (err) {
         res.send('Invalid Token')
