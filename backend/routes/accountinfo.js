6a82f3a0e1dc5e3a131bbaff25dd1301c4fa7488
AlidaSchefers <alidadschefers@gmail.com>
2021-01-23 20:33:46 -0500

    imported changePasswordRoute

--- backend/routes/accountinfo.js
+++ backend/routes/accountinfo.js
@@ -3,7 +3,9 @@ const verify = require('../middlwares/verifyToken')
 const sendErr = require('../middlwares/sendGenericError')
 const User = require('../models/User.js')
 // const jwt = require('jsonwebtoken')
+const changePasswordRouter = require('./changepassword')
 
+//this structure allows someone with an unmodified jwt to send an id and get the account info.
 router.get('/', verify, async (req,res,next) => {
     try {
         // const userId = await jwt.verify(req.body.token, process.env.SECRET_KEY)._id
@@ -15,5 +17,7 @@ router.get('/', verify, async (req,res,next) => {
         sendErr(error, res)
     }
 })
+
+router.use('/changepassword', changePasswordRouter)
 
 module.exports = router
