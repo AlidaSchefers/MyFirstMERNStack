79f6286545ef9a58d61512dd4e1726fbf28cd804
AlidaSchefers <alidadschefers@gmail.com>
2021-01-27 21:29:21 -0500

    added style for navigation buttons

--- frontend/src/config/styles.js
+++ frontend/src/config/styles.js
@@ -1,8 +1,21 @@
 //export object, which will contain sub-objects, which contain css for specific group elements.
 module.exports = {
-    homeTitle: {
+    pageTitle: {
         marginTop: '3%', //3 percent
         fontSize: 'xxx-large'
+    },
+    defaultNavBtn: {
+        borderRadius: 20,
+        fontSize: 30,
+        padding: 15,
+        cursor: 'pointer',
+        transition: 'all ease-in-out .5s',
+        border: 'solid 1px black',
+        textShadow: '1px 1px black',
+        color: 'white',
+        backgroundColor: 'none',
+        marginLeft: "3%",
+        marginRight: "3%",
     }
 }
 
