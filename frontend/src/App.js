ecc20857e50afd342bb51a488c556ce472b3d4c9
AlidaSchefers <alidadschefers@gmail.com>
2021-01-27 21:30:39 -0500

    replaced Home component with Navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import Home from './components/Home'
+import Navigation from './components/Navigation'
 
 function App() {
-  
   return (
-    <Home /> 
+    <Navigation /> 
   );
 }
 
