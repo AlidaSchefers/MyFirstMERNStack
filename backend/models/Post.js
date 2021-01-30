eeee2356ef8709ae07b61d6f1be5d4a8da8d8ed7
AlidaSchefers <alidadschefers@gmail.com>
2021-01-30 09:11:30 -0500

    added schema

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,1 +1,8 @@
-//
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema({
+    postTitle: {type: String, required: true, minlength: 3, maxlength: 250},
+    post: {type: String, required: true, minlength: 1}
+})
+
+module.exports = postSchema.model('post', postSchema)
