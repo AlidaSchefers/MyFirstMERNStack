1aab4c21c5a381664d91442be404eddf36d8fe49
AlidaSchefers <alidadschefers@gmail.com>
2021-01-30 09:10:58 -0500

    token comes from headers and catch sends error status code now

--- backend/middlwares/verifyToken.js
+++ backend/middlwares/verifyToken.js
@@ -3,14 +3,14 @@ const jwt = require('jsonwebtoken')
 //this just verifies that the token had not been modified since it was created. 
 module.exports = function(req, res, next) { //verifies the token
     //get the token
-    const token = req.body.token
+    const token = req.headers['auth-token']
     if(!token) return res.status(401).send('Access Denied')
     try {
         const verified = jwt.verify(token, process.env.SECRET_KEY) //this give us the payload (_id and iat)
         req.user = verified
         next()
     } catch (err) {
-        res.send('Invalid Token')
+        res.status(400).send('Invalid Token')
     }
     //compare the token
     //allow or not.
